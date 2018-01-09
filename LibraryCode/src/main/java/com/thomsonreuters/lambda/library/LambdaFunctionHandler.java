5dd84d5168e6ab2ecb39f6270099ff42b359814b
Dean <Andrew.Dean@thomsonreuters.com>
2018-01-09 11:56:06 +0000

    Initial commit

--- LibraryCode/src/main/java/com/thomsonreuters/lambda/library/LambdaFunctionHandler.java
+++ LibraryCode/src/main/java/com/thomsonreuters/lambda/library/LambdaFunctionHandler.java
@@ -1,17 +1,11 @@
 package com.thomsonreuters.lambda.library;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.thomsonreuters.aws.ec2.IEC2;
 
-public class LambdaFunctionHandler implements RequestHandler<Object, String> {
+public class LambdaFunctionHandler{
 
-    @Override
-    public String handleRequest(Object input, Context context) {
-        context.getLogger().log("Input: " + input);
+	public static void main(String [ ] args) {
         IEC2 ec2Env;
-        // TODO: implement your handler
-        return "Hello from Lambda!";
     }
 
 }

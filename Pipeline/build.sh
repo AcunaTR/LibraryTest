faece61c2cb8250d0836831a59646b618f4e6816
Dean <Andrew.Dean@thomsonreuters.com>
2018-01-09 14:09:47 +0000

    Attempt maven to deploy to s3

--- Pipeline/build.sh
+++ Pipeline/build.sh
@@ -7,8 +7,6 @@ trap buildFail ERROR
 
 CURDIR=`pwd`
 
-aws s3 cp s3://acuna-lambda/Acuna-Library-1.0.0.jar ./LibraryCode/libs
-
 docker run --rm \
            -v "${CURDIR}/:/build" \
            --workdir /build/LibraryCode \

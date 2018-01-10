0a5a7246de52593cc42fc7c1beaae5c38ec71ef2
Pressman <Annette.Pressman@thomsonreuters.com>
2018-01-10 09:02:52 +0000

    test text

--- LibraryCode/src/main/java/com/thomsonreuters/lambda/library/LambdaFunctionHandler.java
+++ LibraryCode/src/main/java/com/thomsonreuters/lambda/library/LambdaFunctionHandler.java
@@ -3,7 +3,7 @@ package com.thomsonreuters.lambda.library;
 import com.thomsonreuters.aws.ec2.IEC2;
 
 public class LambdaFunctionHandler{
-
+//test
 	public static void main(String [ ] args) {
         IEC2 ec2Env;
     }
